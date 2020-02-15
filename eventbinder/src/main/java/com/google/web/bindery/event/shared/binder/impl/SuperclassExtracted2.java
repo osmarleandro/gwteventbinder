d2772d85e5579891295a8c49f831d8b9e16070ae
Osmar Leandro <osmarleandrods@gmail.com>
2020-02-15 14:35:07 -0300

    ref: Move field at SuperClassExtracted2::CONST to AbstractEventBinder with default params

--- eventbinder/src/main/java/com/google/web/bindery/event/shared/binder/impl/SuperclassExtracted2.java
+++ eventbinder/src/main/java/com/google/web/bindery/event/shared/binder/impl/SuperclassExtracted2.java
@@ -4,8 +4,6 @@ import com.google.gwt.event.shared.GwtEvent;
 
 public class SuperclassExtracted2 extends GwtEvent.Type<GenericEventHandler> {
 
-    public static final String CONST = "123465789";
-
     public SuperclassExtracted2() {
         super();
     }
